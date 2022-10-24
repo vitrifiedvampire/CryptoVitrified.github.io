86fec763683922b459d01f361b0283b720451193
Arbab Badar Khan <arbabbadarkhan02@gmail.com>
2022-10-24 14:35:19 +0530

    My App

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+
+require('@nomiclabs/hardhat-waffle');
+ module.exports={
+    solidity:'0.8.0',
+    networks:{
+    goerli:{
+        url:'https://eth-goerli.g.alchemy.com/v2/gAs_Yl2ie1GoqGWD2JwZh6kGLpqUQRxz',
+        accounts:['6a11c1149ed012b9ed72d192a180055dd6b6f905afcd0ce84630bc293e503a2d']
+    }
+    }
+ }
